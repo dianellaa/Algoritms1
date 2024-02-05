aa76608ed22226eebfab3cece488f05a682e7cf9
Dia <157759708+dianellaa@users.noreply.github.com>
2024-02-05 15:45:13 +0100

    Add files via upload

---
+++ Algoritms.js
@@ -0,0 +1,68 @@
+//algoritms 1
+/*
+for (let i = 1; i <= 135; i++){
+    console.log(i);
+}
+*/
+/*Num 2
+let oddNumbers = 'i';
+for(i=1; i<=135; i+=2){
+ console.log(i);
+};
+*/
+/*Num3 
+let sum = 0;
+
+for (let i = 1; i <= 135; i++) {
+  sum += i;
+  console.log(`Number: ${i} | Sum: ${sum}`);
+};
+*/
+//Num-4
+ /*(const X = [1, 4, 2, 12];
+
+for (let i = 0; i < X.length; i++) {
+  console.log(X[i]);
+}
+*/
+//num-5
+/*const x=[-4,-1,0]
+let maxNum=x[0]
+for(num of x){
+    if(num>maxNum){
+      maxNum=num
+    }
+}
+console.log(maxNum);
+*/
+//num6
+/*const X = [2,1.3];
+let sum = 0;
+for (let i = 0; i < X.length; i++) {
+  sum += X[i];
+}
+
+const average = sum / X.length;
+
+console.log(average);
+*/
+//num7
+/*const X = [2, -1, 4, -3];
+
+for (let i = 0; i < X.length; i++) {
+  if (X[i] < 0) {
+    X[i] = 0;
+  }
+}
+
+console.log(X);
+*/
+//num8
+/*let x=[1,4,6,-7,-1,9,8,0,-54]
+for(let i=0; i<=x.length; i++){
+    if(x[i]<0){
+     x[i]= 'turing'
+    }
+}
+console.log(x);
+*/
